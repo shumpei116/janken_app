02990a370a0f7291b824aab24c2712f5a3917bfa
Ubuntu <ubuntu@ip-172-31-45-118.ap-northeast-1.compute.internal>
2021-04-25 21:45:08 +0000

    Fixed janken.rb

--- janken.rb
+++ janken.rb
@@ -1,6 +1,5 @@
 def janken
   jankens= ["グー","チョキ","パー",3]
-  
   puts "0(グー)1(チョキ)2(パー)3(戦わない)"
   
   player_hand =gets.to_i
@@ -10,7 +9,7 @@ def janken
     puts "ホイ！"
     puts "----------"
     puts "あなた：#{jankens[player_hand]}を出しました"
-    puts "相手：#{jankens[program_hand]}を出しました"
+    puts "CP：#{jankens[program_hand]}を出しました"
     puts "----------"
   end
   
@@ -22,11 +21,13 @@ def janken
         player_hand == 1  && program_hand == 0  || 
         player_hand == 2  && program_hand == 1
     @case_pattern = "lose" #プレイヤーの負け
+    puts "CPの勝利！ CPのターン！"
     return false
   elsif player_hand == 0  && program_hand == 1  || 
         player_hand == 1  && program_hand == 2  || 
         player_hand == 2  && program_hand == 0
     @case_pattern = "win" #プレイヤーの勝ち
+    puts "あなたの勝利！ あなたのターン!"
     return false
   elsif player_hand == 3 #戦わない
     puts "平和主義者！"
@@ -42,32 +43,66 @@ def a_hoi
   puts "0(上)1(下)2(左)3(右)"
   choises = ["上","下","左","右"]
   
-  player_direction = gets.to_i
-  program_direction = rand(4)
+  @player_direction = gets.to_i
+  @program_direction = rand(4)
   
   puts "ホイ！"
 
-  if (0..3) === player_direction
+  if (0..3) === @player_direction
     puts "----------"
-    puts "あなた：#{choises[player_direction]}"
-    puts "相手：#{choises[program_direction]}"
+    puts "あなた：#{choises[@player_direction]}"
+    puts "CP：#{choises[@program_direction]}"
     puts "----------"
   else
     puts "もう一度やり直してください"
+    return
   end
 end
 
-next_janken = true
-
-puts "じゃんけん..."
-
-while next_janken do
-  next_janken = janken
-end
-
-case @case_pattern
-  when "win","lose"
-    a_hoi
+def a_hoi_result
+  if @player_direction == @program_direction
+    @result = 0
   else
-    return
+    @result = 1
+  end
+    
+  case @case_pattern
+    when "win"
+      if @result == 0
+        puts "あなたの勝利！！！"
+      @endressgame = false
+      elsif @result == 1
+        puts "もう一回！"
+        @endressgame = true
+      end
+    when "lose"
+      if @result == 0
+        puts "CPの勝利！！！"
+      @endressgame = false
+      elsif @result == 1
+        puts "もう一回！"
+        @endressgame = true
+      end
+  end
+end
+  
+
+@endressgame = true
+
+while @endressgame do
+  next_janken = true
+  
+  puts "じゃんけん..."
+  
+  while next_janken do
+    next_janken = janken
+  end
+  
+  case @case_pattern
+    when "win","lose"
+      a_hoi
+      a_hoi_result
+    else
+      return
+  end
 end
